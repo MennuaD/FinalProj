f59b0b8e1f11f4a2cb2e1dddfcf31345cf664fce
unknown <BT_4N2_18@BKTECH.LOCAL>
2023-05-01 12:33:52 -0400

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,13 @@
+public class Board {
+        private PathA pathA;
+        private PathB pathB;
+        private PathC pathC;
+        private PathD pathD;
+        private PathE pathE;
+
+
+        public Board(PathA a, PathB b, PathC c, PathD d, PathE e){
+                
+        }
+
+}
---
+++ src/PathE.java
@@ -0,0 +1,2 @@
+public class PathE {
+}
