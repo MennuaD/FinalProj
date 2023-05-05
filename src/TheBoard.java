f7bc3f9dfefc27ef3426dc03466f895633ac162f
MennuaD <mdavis0585@bths.edu>
2023-05-05 13:24:57 -0400

    Morerer

--- src/TheBoard.java
+++ src/TheBoard.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 
 public class TheBoard {
     private JPanel panel1;
+    private JButton a2PlayerButton;
+    private JButton practice1PlayerButton;
+    private JButton a3PlayerButton;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
