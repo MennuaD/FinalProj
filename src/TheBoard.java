3c564597c22e8afc2ed8d0eb07d50f151203e30e
MennuaD <mdavis0585@bths.edu>
2023-05-08 10:45:54 -0400

    Morererer

--- src/TheBoard.java
+++ src/TheBoard.java
@@ -4,7 +4,6 @@ public class TheBoard {
     private JPanel panel1;
     private JButton a2PlayerButton;
     private JButton practice1PlayerButton;
-    private JButton a3PlayerButton;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
