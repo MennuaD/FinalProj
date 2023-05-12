984f8c47bdbfe0b40d5cb2302e7f4ca90994b6c0
MennuaD <mdavis0585@bths.edu>
2023-05-12 12:35:32 -0400

    Morerererer

--- src/TheBoard.java
+++ src/TheBoard.java
@@ -1,6 +1,14 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.Font;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 
 public class TheBoard extends JFrame {
     private JPanel panel1;
@@ -9,7 +17,11 @@ public class TheBoard extends JFrame {
     private JTextArea textArea1;
 
     public TheBoard() {
-
+    setContentPane(panel1);
+    setTitle("Main Page");
+    setSize(400, 400);
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    setVisible(true);
     }
 
     private void createUIComponents() {
