7c66ee6bd41053cbfe79aa802e0d8e1729381c6c
MennuaD <mdavis0585@bths.edu>
2023-05-22 12:34:04 -0400

    I found some images I might use in my project and I figured out how to add an image in a gui.

---
+++ src/AnImage.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class AnImage {
+    private JPanel panel1;
+}
--- src/PowerUps.java
+++ src/PowerUps.java
@@ -1,3 +1,7 @@
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+
 public class PowerUps {
 
 
